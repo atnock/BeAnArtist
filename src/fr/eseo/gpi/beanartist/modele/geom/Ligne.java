9cfb2577ab4f2839529aad7c10598281c8a5c8be
atnock <pecolas44@gmail.com>
2016-04-26 13:59:07 +0200

    ligne validate

--- src/fr/eseo/gpi/beanartist/modele/geom/Ligne.java
+++ src/fr/eseo/gpi/beanartist/modele/geom/Ligne.java
@@ -2,7 +2,7 @@
 
 
  public class Ligne extends Forme{
-	 public double EPSILON = 0.1;
+	 public static final double EPSILON = 0.5;
 	 
 	 public Ligne(){
 		 this(new Point(), 0, 0);
@@ -29,8 +29,56 @@
 	 }
 	 
 	 public Point getP1(){
-		 return 
+		 return this.getPosition();
 	 }
+	 
+	 public Point getP2(){
+		 Point P1 = this.getP1();
+		 return new Point(P1.getX() + this.getLargeur(), P1.getY() + this.getHauteur());
+	 }
+	 
+	 public void setP1(Point newPoint){
+		 this.getP1().setX(newPoint.getX());
+		 this.getP1().setY(newPoint.getY());
+	 }
+	 
+	 public void setP2(Point newPoint){
+		 Point P1 = this.getP1();
+		 this.setHauteur(newPoint.getX() - P1.getX());
+		 this.setLargeur(newPoint.getY() - P1.getY());
+	 }
+	 
+	 public int getMaxX(){
+			int max = getX()+this.getLargeur();
+			if (max<this.getX()){
+				max=this.getX();
+			}
+			return max;
+		}
+
+		public int getMaxY(){
+			int max = getY()+this.getHauteur();
+			if (max<this.getY()){
+				max=this.getY();
+			}
+			return max;
+		}
+
+		public int getMinX(){
+			int min = getX();
+			if (min>this.getX()+this.getLargeur()){
+				min=this.getX()+this.getLargeur();
+			}
+			return min;
+		}
+		
+		public int getMinY(){
+			int min = getY();
+			if (min>this.getY()+this.getHauteur()){
+				min=this.getY()+this.getHauteur();
+			}
+			return min;
+		}
 	 
 	 public double périmètre(){
 		 int x = this.getLargeur();
@@ -38,12 +86,25 @@
 		 return Math.sqrt(x*x+y*y);
 	 }
 	 
-	 public boolean contient(int x, int y){
-		 return false;
+	 private double sqr(double x){
+		 return x*x;
 	 }
 	 
-	 public boolean contient(Point testPosition){
-		 return false;
+	 private double distance(int x1, int x2, int y1, int y2){
+		 return Math.sqrt(sqr(y2 - y1) + sqr(x2 - x1));
+	 }
+	 
+	 public boolean contient(int x, int y){
+		 return contient(new Point(x,y));
+	 }
+	 
+	 public boolean contient(Point P){
+		 Point P1 = this.getP1();
+		 Point P2 = this.getP2();
+		 double distanceP1P = Math.abs(distance(P1.getX(), P.getX(), P1.getY(), P.getY()));
+		 double distanceP2P = Math.abs(distance(P.getX(), P2.getX(), P.getY(), P2.getY()));
+		 double distanceP1P2 = Math.abs(distance(P1.getX(), P2.getX(), P1.getY(), P2.getY()));
+		 return distanceP1P + distanceP2P - distanceP1P2 <= EPSILON;
 	 }
 	 
 	 public String toString(){
