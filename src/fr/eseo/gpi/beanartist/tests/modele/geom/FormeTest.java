745d90201cf38d498b15f87fcd3e64f9722d527f
courde <carolinecourde@hotmail.fr>
2016-04-26 14:16:16 +0200

    petit test a la ...
    Signed-off-by: courde <carolinecourde@hotmail.fr>

---
+++ src/fr/eseo/gpi/beanartist/tests/modele/geom/FormeTest.java
@@ -0,0 +1,11 @@
+package fr.eseo.gpi.beanartist.tests.modele.geom;
+import fr.eseo.gpi.beanartist.modele.geom.Forme;
+
+
+public class FormeTest {
+	public static void main(String [] args){
+		Forme forme1= new Forme();
+		System.out.println(forme1.toString());
+		
+	}
+}
